8fd50d829ee89be27b0e4224dd688491f50fd513
Tekorita <davidpalmalugo@gmail.com>
2019-07-21 16:08:42 -0400

    cliente servidor con node js y express

---
+++ variables/strings.js
@@ -0,0 +1,22 @@
+//Strings
+
+var nombre = 'david', apellido = 'palma'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = nombre + ' ' + apellido
+
+var nombreCompleto2 = `${nombre} ${apellido}`
+
+var str = nombre.charAt(1) + nombre.charAt(2)
+
+var str2 = nombre.substr(1, 2)
+
+var ultimaPosicion = nombre.length - 1
+
+var ultimaLetraDelNombre = nombre.charAt(ultimaPosicion)
