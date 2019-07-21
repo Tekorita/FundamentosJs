daf7e7409b0f88518521d5285370a524a571734f
Tekorita <davidpalmalugo@gmail.com>
2019-07-21 17:55:35 -0400

    parametros por referencia de objetos y funciones

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -34,9 +34,25 @@ function imprimirNombreYEdad(persona)
     console.log(`${nombre} ${apellido} tiene ${edad} años`)
 }
 
+//Parametros como referencia o como valor donde una funcion recibe
+//como parametros un objeto y retorna un nuevo objeto sin modificar
+//el atributo del objeto inicial
+
+function cumpleaños(persona)
+{
+    return {
+        //... es para copiar o desglozar el objeto
+        ...persona, 
+        edad: persona.edad +1
+    }
+}
+
 imprimirNombreEnMayusculas(david)
 imprimirNombreEnMayusculas(daniela)
 imprimirNombreEnMayusculas2({ nombre: 'Mierdin'})
 
+//ahora cuando imprimimos esta funcion modificamos el atributo
+//edad del objeto david pero si despues de eso llamamos al objeto david
+//vemos que el atributo no se modifico
 imprimirNombreYEdad(david)
 
