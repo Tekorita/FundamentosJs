7e1f22bdb1a3a1faf01d350c213ce7047a49f51c
Tekorita <davidpalmalugo@gmail.com>
2019-07-21 17:02:17 -0400

    ejemplo de funciones

---
+++ funciones/alcanceDeFunciones.js
@@ -0,0 +1,9 @@
+var nombre = 'David'
+
+function imprimirNombreEnMayusculas(n) 
+{
+    n = n.toUpperCase()
+    console.log(n)
+}
+
+imprimirNombreEnMayusculas(nombre)
---
+++ funciones/funciones.js
@@ -0,0 +1,9 @@
+var nombre = "David", edad = 29
+
+function imprimirEdad(n, e)
+{
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+
