52d6c51cec59334604874efe6e7c3c0ddccf3d67
Tekorita <davidpalmalugo@gmail.com>
2019-07-22 00:26:36 -0400

    restructuracion de carpetas del curso

---
+++ 2-estructurasDeControlYFunciones/switch/switch.js
@@ -0,0 +1,19 @@
+var signo = prompt ("¿Cuál es tu signo?")
+
+switch (signo) {
+    case 'geminis':
+        console.log('Su signo es geminis')
+        break
+    case 'leo':
+        console.log('Su signo es leo')
+        break
+    case 'acuario':
+        console.log('Su signo es acuario')
+        break
+    case 'escorpio':
+        console.log('Su signo es acuario')
+        break
+    default:
+        console.log('no encontramos su signo zodiacal')
+        break
+}
